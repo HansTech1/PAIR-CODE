90ae623e113b2608d590cf4fe082bf6a1ed69302
HansTech1 <hahahamdr25@gmail.com>
2025-05-23 06:58:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
 const express = require('express');
 const app = express();
-__path = process.cwd()
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 7860;
-let server = require('./qr'),
-    code = require('./pair');
+const path = require('path');
+__path = process.cwd();
+
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// Debug: Catch uncaught errors
+process.on('uncaughtException', err => {
+  console.error("Uncaught Exception:", err);
+});
+process.on('unhandledRejection', err => {
+  console.error("Unhandled Rejection:", err);
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+let server, code;
+
+try {
+  server = require('./qr');
+  code = require('./pair');
+  app.use('/server', server);
+  app.use('/code', code);
+} catch (e) {
+  console.error("Error loading routes:", e);
+}
+
+app.use('/pair', (req, res) => {
+  res.sendFile(path.join(__path, 'pair.html'));
+});
+app.use('/qr', (req, res) => {
+  res.sendFile(path.join(__path, 'qr.html'));
+});
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__path, 'main.html'));
+});
+
 app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star
+  console.log(`
+Don't Forget To Give Star!
 
- Server running on http://localhost:` + PORT)
-})
+Server running on http://localhost:` + PORT);
+});
 
-module.exports = app
+module.exports = app;
